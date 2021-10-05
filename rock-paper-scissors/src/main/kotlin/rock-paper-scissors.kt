7b6c04e7b3bfcae138328194cdc8ac5eb00f5e8a
RobBosmans <rob.bosmans@student.ehb.be>
2021-10-05 17:21:16 +0200

    rock-paper-scissors final

--- rock-paper-scissors/src/main/kotlin/rock-paper-scissors.kt
+++ rock-paper-scissors/src/main/kotlin/rock-paper-scissors.kt
@@ -3,15 +3,26 @@ import kotlin.random.Random
 fun main(){
     val array = arrayOf("rock", "paper", "scissors")
     val randomIndex = Random.nextInt(array.size);
-    val randomElement = array[randomIndex]
+    val computerChoice = array[randomIndex]
 
-    print("Your choice: ")
-    val stringInput = readLine()!!
-    //println("You entered: $stringInput")
-    //println("Computer choice " + randomElement)
+    println("Your choice: ")
+    val userChoice = readLine()!!
+    determineWinner(computerChoice, userChoice)
+    println("computer choice was: $computerChoice")
+}
 
-    if (randomElement == stringInput){
-        println("try again")
+fun determineWinner(computerChoice: String, userChoice: String?){
+    if(computerChoice == userChoice){
+        println("draw")
+        return
     }
 
+    val winMessage = "you won"
+    val loseMessage = "you lost"
+
+    when(computerChoice){
+        "rock" -> if (userChoice == "paper") println(winMessage) else println(loseMessage)
+        "paper" -> if (userChoice == "scissors") println(winMessage) else println(loseMessage)
+        "scissors" -> if (userChoice == "rock") println(winMessage) else println(loseMessage)
+    }
 }
