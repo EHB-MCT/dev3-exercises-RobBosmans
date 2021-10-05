5dc0f7e995d3ec8f9df2b01900541a66706d3deb
RobBosmans <rob.bosmans@student.ehb.be>
2021-10-05 16:31:25 +0200

    hello-world

--- hello-world/src/main/kotlin/main.kt
+++ hello-world/src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+val array = arrayOf<String>("yassin", "amin", "Tuur", "Cosima")
+
 fun main(){
     println("hello world")
+    greeting()
+}
+
+fun greeting(){
+    for (element in array){
+        println("hello " + element)
+    }
 }
