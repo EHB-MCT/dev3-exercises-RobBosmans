04ddfd9426ec06de38d123dae62c3482c0b2874c
RobBosmans <rob.bosmans@student.ehb.be>
2021-10-05 17:07:28 +0200

    rock-paper-scissors-base

--- rock-paper-scissors/src/main/kotlin/rock-paper-scissors.kt
+++ rock-paper-scissors/src/main/kotlin/rock-paper-scissors.kt
@@ -1,2 +1,17 @@
-val array = arrayOf("rock", "paper", "scissors")
+import kotlin.random.Random
 
+fun main(){
+    val array = arrayOf("rock", "paper", "scissors")
+    val randomIndex = Random.nextInt(array.size);
+    val randomElement = array[randomIndex]
+
+    print("Your choice: ")
+    val stringInput = readLine()!!
+    //println("You entered: $stringInput")
+    //println("Computer choice " + randomElement)
+
+    if (randomElement == stringInput){
+        println("try again")
+    }
+
+}
