693119c7d7aa4fc5e3e183ba86c0c79484f87d06
RobBosmans <rob.bosmans@student.ehb.be>
2021-10-12 16:42:46 +0200

    3

--- session2/minigame/src/main/kotlin/minigame.kt
+++ session2/minigame/src/main/kotlin/minigame.kt
@@ -6,9 +6,21 @@ fun main(){
 
 fun challengeOne(){
     println("Which animal is Dr Jones afraid of?")
-    println("A.Scorpions, B.Spiders, C.Snakes")
-    val array = arrayOf("Scorpions", "Spiders", "Snakes")
+    println("1. scorpions, 2. spiders, 3. snakes")
+    val options = arrayOf("scorpions", "spiders", "snakes")
+    println("Your choice: ")
+    val userChoice = readLine()!!
+    if (userChoice == options[2]){
+        println("${options[2]} is correct")
+        println("CORRECT")
+    }
+    else{
+        gameOver()
+    }
+}
 
+fun correct(){
+    println("CORRECT")
 }
 
 fun gameOver(){
