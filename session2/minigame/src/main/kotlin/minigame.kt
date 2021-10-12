de909f7f8e32f85c0d4f66b3539b35d1d033b9e8
RobBosmans <rob.bosmans@student.ehb.be>
2021-10-12 15:41:30 +0200

    2

--- session2/minigame/src/main/kotlin/minigame.kt
+++ session2/minigame/src/main/kotlin/minigame.kt
@@ -1,4 +1,16 @@
 
 fun main(){
-    println("Your choice: ")
+    println("Welcome to Indiana Jones TM")
+    challengeOne()
+}
+
+fun challengeOne(){
+    println("Which animal is Dr Jones afraid of?")
+    println("A.Scorpions, B.Spiders, C.Snakes")
+    val array = arrayOf("Scorpions", "Spiders", "Snakes")
+
+}
+
+fun gameOver(){
+    println("GAME OVER")
 }
