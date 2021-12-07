b08d55636a490bc1db080adcc2a50c40d984c872
RobBosmans <rob.bosmans@student.ehb.be>
2021-12-07 14:13:38 +0100

    session 6

--- session6/trains/src/main/kotlin/main.kt
+++ session6/trains/src/main/kotlin/main.kt
@@ -12,14 +12,15 @@ fun main() {
     val result = executeQuery(connection,
 
         //TODO
-        "SELECT rides.*, platforms.name AS platform_name FROM rides " +
-                "INNER JOIN cities ON rides.destination_city_id = cities.id " +
-                "INNER JOIN platforms ON rides.departure_platform_id = platforms.id " +
-                "WHERE cities.name = ? " +
-                "AND rides.departure_time > NOW() " +
-                "AND rides.departure_time < (CURDATE() + INTERVAL 1 DAY) " +
-                "ORDER BY rides.departure_time ASC " +
-                "LIMIT 3;",
+        "SELECT departure_city FROM rides WHERE id = 1",
+                //"SELECT rides.*, platforms.name AS platform_name FROM rides " +
+                //"INNER JOIN cities ON rides.destination_city_id = cities.id " +
+                //"INNER JOIN platforms ON rides.departure_platform_id = platforms.id " +
+                //"WHERE cities.name = ? " +
+                //"AND rides.departure_time > NOW() " +
+                //"AND rides.departure_time < (CURDATE() + INTERVAL 1 DAY) " +
+                //"ORDER BY rides.departure_time ASC " +
+                //"LIMIT 3;",
         destination)
 
     while (result!!.next()) {
